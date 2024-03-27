f6d70300a531e795ad59bf563732a7ae31b66dac
Cucu Toma Daniel <cucutomadaniel@gmail.com>
2024-03-27 19:04:58 +0200

    add input fields and the empty table

--- mission-planner/src/app/planner/planner.component.ts
+++ mission-planner/src/app/planner/planner.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
+interface Point {
+  id: number;
+  name: string;
+  x: number;
+  y: number;
+}
 @Component({
   selector: 'app-planner',
   templateUrl: './planner.component.html',
-  styleUrls: ['./planner.component.css']
+  styleUrls: ['./planner.component.css'],
 })
 export class PlannerComponent {
-
+  point: Point = { id: 0, name: '', x: 0, y: 0 };
+  dataSource = new MatTableDataSource<Point>([]);
+  displayedColumns: string[] = ['id', 'name', 'x', 'y'];
+  addPoint() {}
 }
